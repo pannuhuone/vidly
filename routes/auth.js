7cbcbdd6f47fe9178f91efe4bd17f7248af4d34b
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-02 19:47:47 +0300

    generating token and send back when logged in

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const Joi = require('@hapi/joi');
 const _ = require('lodash');
@@ -17,7 +18,8 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  res.send(token);
 });
 
 function validate(user) {
