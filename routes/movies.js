c96f69aeb936c51943852c0d267b5a3182207252
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-02 07:15:11 +0300

    changes in movie route

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre');
 
 // API: Get all movies
 router.get('/', async (req, res) => {
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
   res.send(movies);
 });
 
-// API: Get one customer
+// API: Get one movie
 router.get('/:id', async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
     return res.status(400).send('Given movie ID is not valid!');
@@ -26,9 +27,15 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre.');
+
   let movie = new Movie({
     title: req.body.title,
-    genre: req.body.genre,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate,
   });
@@ -50,11 +57,17 @@ router.put('/:id', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send('Invalid genre.');
+
   const movie = await Movie.findByIdAndUpdate(
     req.params.id,
     {
       title: req.body.title,
-      genre: req.body.genre,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
       numberInStock: req.body.numberInStock,
       dailyRentalRate: req.body.dailyRentalRate,
     },
