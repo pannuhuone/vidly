895fb29ffa4edfea3e2dbd9f1c3e8052b801dcb7
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-02 07:22:45 +0300

    separated genre schema and model

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('@hapi/joi');
 
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 3,
-      maxlength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 // Genre input validation for inserting new genre to DB.
 function validateGenre(genre) {
@@ -23,4 +22,5 @@ function validateGenre(genre) {
 }
 
 exports.Genre = Genre;
+exports.genreSchema = genreSchema;
 exports.validate = validateGenre;
