520270c1db14318b955885d976a20761be49a9b9
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-03 23:00:46 +0300

    startup config added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const winston = require('winston');
 const morgan = require('morgan');
-const config = require('config');
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -9,11 +8,7 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/database')();
-
-if (!config.get('jwtPrivateKey')) {
-  winston.error('FATAL ERROR: jwtPrivateKey is not defined!');
-  process.exit(1);
-}
+require('./startup/config')();
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
