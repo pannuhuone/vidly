9de19461f352a46b3c257237a0141b2aeb00f105
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-18 17:52:10 +0300

    tests for post genre+genre model validation fixes

--- models/genre.js
+++ models/genre.js
@@ -5,7 +5,7 @@ const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 3,
+    minlength: 5,
     maxlength: 50,
   },
 });
@@ -15,7 +15,7 @@ const Genre = mongoose.model('Genre', genreSchema);
 // Genre input validation for inserting new genre to DB.
 function validateGenre(genre) {
   const JoiSchema = Joi.object({
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(5).max(50).required(),
   }).options({ abortEarly: false });
 
   return JoiSchema.validate(genre);
--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
-const {Genre} =require('../../models/genre')
+const {Genre} =require('../../models/genre');
+const {User} =require('../../models/user');
 
 let server;
 
@@ -48,25 +49,65 @@ describe('/api/genres', () => {
       expect(res.status).toBe(404);
     })
 
-    // it('should return 400 if invalid id is used for getting one genre', async () => {
-    //   await Genre.collection.insertMany([
-    //     {name: 'genre1'},
-    //     {name: 'genre2'},
-    //   ])
 
-    //   const res = await request(server).get('/api/genres/1');
-    //   expect(res.status).toBe(400);
-    // })
-
-    // it('should return 404 if genre cannot be found', async () => {
-    //   await Genre.collection.insertMany([
-    //     {name: 'genre1'},
-    //     {name: 'genre2'},
-    //   ])
-
-    //   const res = await request(server).get('/api/genres/5ee5315bafeddc665428bf89');
-    //   expect(res.status).toBe(404);
-    // })
   });
-});
 
+  describe('POST /', () => {
+    it('should return 404 if client is not logged in', async () => {
+      const res = await request(server)
+        .post('/api/genres')
+        .send({ name: 'genre1'});
+
+      expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if genre is less than 5 characters', async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post('/api/genres')
+        .set('x-auth-token', token)
+        .send({ name: '1234'});
+
+      expect(res.status).toBe(400);
+    });
+
+    it('should return 400 if genre is more than 50 characters', async () => {
+      const token = new User().generateAuthToken();
+      const name = new Array(52).join('a');
+
+      const res = await request(server)
+        .post('/api/genres')
+        .set('x-auth-token', token)
+        .send({ name: name });
+
+      expect(res.status).toBe(400);
+    });
+
+    it('should save the genre if it is valid', async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post('/api/genres')
+        .set('x-auth-token', token)
+        .send({ name: 'genre1' });
+
+      const genre = await Genre.find({ name: 'genre1' });
+
+      expect(genre).not.toBeNull();
+    });
+
+    it('should return the genre if it is valid', async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post('/api/genres')
+        .set('x-auth-token', token)
+        .send({ name: 'genre1' });
+
+      expect(res.body).toHaveProperty('_id');
+      expect(res.body).toHaveProperty('name', 'genre1');
+    });
+  });
+
+});
