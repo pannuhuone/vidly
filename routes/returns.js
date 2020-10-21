ce5dc06997fa8953e79f8bf89e19fadcd15a3bb0
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-21 20:26:50 +0300

    mongoose static method refactoring

--- routes/returns.js
+++ routes/returns.js
@@ -10,9 +10,8 @@ const validate = require('../middleware/validate');
 
 // POST /api/returns {customerId, movieId}
 router.post('/', [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId, 
-    'movie._id': req.body.movieId});
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
+  
   if (!rental) return res.status(404).send('rental not found');
 
   if(rental.dateReturned) return res.status(400).send('Return already processed');
